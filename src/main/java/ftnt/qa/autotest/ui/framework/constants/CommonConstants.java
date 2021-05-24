0adc0b435ee180983d700608fcaf1c57efd933ba
mhmoudsabrykdg9g <wrq_1976@yeah.net>
2021-05-24 17:57:47 +0800

    ADD browser drivers

--- src/main/java/ftnt/qa/autotest/ui/framework/constants/CommonConstants.java
+++ src/main/java/ftnt/qa/autotest/ui/framework/constants/CommonConstants.java
@@ -1,12 +1,14 @@
 package ftnt.qa.autotest.ui.framework.constants;
 
 public class CommonConstants {
-	public static final String  IE_DRIVER_FIELD="123";
-	public static final String  IE_DRIVER_PATH="123";
+	public static final String  IE_DRIVER_FIELD="webdriver.ie.driver";
+	public static final String  IE_DRIVER_PATH="/BrowserDriver/IEDriverServer.exe";
 	
-	public static final String  CHROME_DRIVER_FIELD="123";
-	public static final String  CHROME_DRIVER_PATH="123";
+	public static final String  CHROME_DRIVER_FIELD="webdriver.chrome.driver";
+	public static final String  CHROME_DRIVER_PATH="/BrowserDriver/chromedriver.exe";
 	
+	public static final String  FIREFOX_DRIVER_FIELD="webdriver.gecko.driver";
+	public static final String  FIREFOX_DRIVER_PATH="/BrowserDriver/geckodriver.exe";
 	
 	public static final Integer RETRY_COUNT=3;
 	
--- src/main/java/ftnt/qa/autotest/ui/framework/webdriverfactory/FireFoxBrowserDriver.java
+++ src/main/java/ftnt/qa/autotest/ui/framework/webdriverfactory/FireFoxBrowserDriver.java
@@ -2,17 +2,18 @@ package ftnt.qa.autotest.ui.framework.webdriverfactory;
 
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import ftnt.qa.autotest.ui.framework.constants.CommonConstants;
 import ftnt.qa.autotest.ui.framework.interfaces.IBrowserDriverBase;
 
 public class FireFoxBrowserDriver implements IBrowserDriverBase {
 
 	public void configBrowser() throws Exception {
-		// TODO Auto-generated method stub
+		System.setProperty(CommonConstants.FIREFOX_DRIVER_FIELD, CommonConstants.FIREFOX_DRIVER_PATH);
 	}
 
 	@SuppressWarnings("unchecked")
 	public <T> T getWebDriver() throws Exception {
-		// TODO Auto-generated method stub
+		configBrowser();
 		return (T) new FirefoxDriver();
 	}
 }
