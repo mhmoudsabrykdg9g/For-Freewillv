0fedb2bdded3d02046eab70494a76e8dc0deb5ef
mhmoudsabrykdg9g <wrq_1976@yeah.net>
2021-05-30 16:39:59 +0800

    add test samples

--- src/main/java/ftnt/qa/autotest/ui/fortisiem/testcases/LoginTests.java
+++ src/main/java/ftnt/qa/autotest/ui/fortisiem/testcases/LoginTests.java
@@ -1,10 +1,14 @@
 package ftnt.qa.autotest.ui.fortisiem.testcases;
 
+import java.lang.reflect.Method;
+
 import org.openqa.selenium.support.PageFactory;
 import ftnt.qa.autotest.ui.framework.testng.listener.RetryToRunCase;
 import org.testng.Assert;
+import org.testng.ITestContext;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import org.testng.xml.XmlTest;
 
 import ftnt.qa.autotest.ui.fortisiem.pageobject.Login;
 import ftnt.qa.autotest.ui.testbase.TestBase;
@@ -12,7 +16,8 @@ import ftnt.qa.autotest.ui.testbase.TestBase;
 /**
  * Created by ssuku on 1/9/17.
  */
-public class LoginTests extends BaseTest {
+
+public class LoginTests extends TestBase {
 
     // Login test
     @Parameters({"username", "password", "custId", "domain"})
@@ -50,4 +55,40 @@ public class LoginTests extends BaseTest {
         loginObj.logout();
         Assert.assertFalse(loginObj.isLoggedIn(), "logout failed");
     }
+
+	@Override
+	public void terminTestSuite(XmlTest xt, ITestContext tc) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeClass(XmlTest xt, ITestContext tc) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterClass(XmlTest xt, ITestContext tc) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeTest(XmlTest xt, ITestContext tc) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterTest(XmlTest xt, ITestContext tc) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initDriver(ITestContext tc, Method m, XmlTest xt) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 }
